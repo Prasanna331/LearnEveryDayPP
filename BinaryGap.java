03f778c3e5e8c602dca006b06d3c2284fe382628
Prasanna331 <61084842+Prasanna331@users.noreply.github.com>
2026-05-07 09:58:35 +0530

    Implement binary gap calculation in BinaryGap.java

--- BinaryGap.java
+++ BinaryGap.java
@@ -1,4 +1,21 @@
 Class Solution{
   public int binaryGap(int N){
-    
+    int maxGap = 0;
+    int currentGap =0;
+    boolean counting = false;
+   while(N > 0){
+     if(N & 1 == 1){
+     if(counting){
+       maxGap = Integer.max(currentGap, maxGap);
+     }
+       counting = true;
+       currentGap = 0;
+     }
+     else if(counting){
+       currentGap++;
+     }
+     N >>= 1;
+   }
+    return maxGap;
+  }   
 }
