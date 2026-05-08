af7ab860611dbe75cb493eb4898056e532db8cb9
Prasanna331 <61084842+Prasanna331@users.noreply.github.com>
2026-05-08 06:50:49 +0530

    Add cyclic rotation method for an array
    Implement cyclic rotation of an array by k positions.

--- cyclicRotationArray.java
+++ cyclicRotationArray.java
@@ -1,5 +1,11 @@
 Class Solution{
   public int[] cyclicrotation(int k, int[] A)
     {
+    int[] res = new int[A.length];
+    for(int i =0; i<res.length;i++){
+      res[(i+k)%A.length] = A[i];
+    }
+    return res;
+      
     }
 }
